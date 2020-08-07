97ea1d79e062ea49b452fe502c10f7f1822cc422
Sohail Karigar <sohail.karigar@hcl.com>
2020-08-07 22:26:19 +0530

    learn

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import(
+	"fmt"
+	"proj1/upp"
+)
+func main()  {
+	fmt.Println(upp.Upp("sal"))
+}
---
+++ upp/upp.go
@@ -0,0 +1,5 @@
+package upp
+import "strings"
+func Upp(s string) string  {
+	return strings.ToUpper(s)
+}
